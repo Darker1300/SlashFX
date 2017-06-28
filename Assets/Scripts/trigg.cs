1b9d9156273768c751d63c90c0d0b3134a6b7a9e
Ethan Moffatt <darker1300@gmail.com>
2017-06-29 02:30:48 +1000

    Added Project.

---
+++ Assets/Scripts/trigg.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class trigg : MonoBehaviour
+{
+    public bool repeat = false;
+    public string triggerName = "Slash";
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (repeat || Input.GetMouseButtonDown(0))
+        {
+            anim.ResetTrigger(triggerName);
+            anim.SetTrigger(triggerName);
+        }
+    }
+}
